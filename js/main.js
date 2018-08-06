b041c7ff7293699c9cfcbf9bd659a867a9e5f3b5
Umpoy <iumpoy@yahoo.com>
2018-08-06 16:54:33 -0700

    cookies increment when bought from store

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
 var cookies = 0;
-
+var cookiesPerSecond = 0;
+//quantity
+var cursor = {
+    baseCost: 15,
+    quantity: 0,
+    CPS: 0.1
+}
 $(document).ready(init);
 
-
 function init() {
+    renderCookies();
     assignClickHandler();
+    setInterval(function () {
+        autoAddCookies();
+    }, 1000)
 }
 
 function assignClickHandler() {
     $(".cookie").on("click", function () {
         cookies++;
-        render_cookies();
+        renderCookies();
+    });
+    $(".cursor").on("click", function () {
+        cursor.quantity++;
+        cookiesPerSecond += cursor.CPS;
+        console.log(cursor);
+        console.log(cookiesPerSecond);
+
     })
 }
 
-function render_cookies() {
-    $(".cookieAmount").html(cookies);
+function renderCookies() {
+    $(".cookieAmount").html(Math.round(cookies));
+}
+
+function cookieCostFormula(obj) {
+    return obj.baseCost * Math.pow(1.1, obj.quantity)
+}
+
+function autoAddCookies() {
+    cookies += cookiesPerSecond;
+    renderCookies();
 }
