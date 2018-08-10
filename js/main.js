9409c71e25dde20da3e1767bb861798c85eb59cb
Umpoy <iumpoy@yahoo.com>
2018-08-10 11:49:38 -0700

    unlock items in store when cookies hit a certain number

--- js/main.js
+++ js/main.js
@@ -31,13 +31,15 @@ function assignClickHandler() {
 }
 
 function renderToDOM() {
-    // unlockStore();
+    console.log(cookies);
+    unlockStore();
     $(".cookieAmount").html(Math.round(cookies));
     $(".CPS").html(cookiesPerSecond);
 }
 
 function cookieCostFormula(obj) {
-    return obj.baseCost * Math.pow(1.1, obj.quantity)
+
+    return parseInt((obj.baseCost * Math.pow(1.1, obj.quantity)).toFixed(1));
 }
 
 function autoAddCookies() {
@@ -45,6 +47,29 @@ function autoAddCookies() {
     renderToDOM();
 }
 
-// function unlockStore(){
-
-// }
+function unlockStore() {
+    if (cookies >= cursor.baseCost) {
+        $(".cursor").removeClass("hidden");
+    }
+    if (cookies >= grandma.baseCost) {
+        $(".grandma").removeClass("hidden")
+    }
+    if (cookies >= factory.baseCost) {
+        $(".factory").removeClass("hidden")
+    }
+    if (cookies >= mine.baseCost) {
+        $(".mine").removeClass("hidden")
+    }
+    if (cookies >= shipment.baseCost) {
+        $(".shipment").removeClass("hidden")
+    }
+    if (cookies >= lab.baseCost) {
+        $(".lab").removeClass("hidden")
+    }
+    if (cookies >= portal.baseCost) {
+        $(".portal").removeClass("hidden")
+    }
+    if (cookies >= timemachine.baseCost) {
+        $(".timemachine").removeClass("hidden")
+    }
+}
